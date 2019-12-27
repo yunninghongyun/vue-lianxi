f09bac8f1daf86e9419934c66ca30cdac74da364
yunninghongyun <786898343@qq.com>
2019-12-27 16:51:11 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="wrap">
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+    <div class="block">
+      <el-date-picker v-model="value1" type="date" placeholder="选择日期">
+      </el-date-picker>
+    </div>
+    <Button />
+    <ell />
+  </div>
+</template>
+
+<script>
+import ell from "./components/Element.vue";
+import Button from "./components/Button";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      // 创建了一个变量 bool     const bool = false
+      bool: true,
+      bool1: "red",
+      name: "",
+      styles: {
+        color: "red"
+      },
+      pickerOptions1: {
+        disabledDate(time) {
+          return time.getTime() > Date.now();
+        }
+      },
+      value1: ""
+    };
+  },
+  components: {
+    ell,
+    Button
+  }
+};
+</script>
+
+<style>
+.active {
+  color: red;
+}
+.bg {
+  color: blue;
+  /* font-size: 100px; */
+}
+</style>
---
+++ src/elementui.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import ElementUI from "element-ui";
+import { Button, DatePicker, Table } from "element-ui";
+Vue.use(Button);
+Vue.use(ElementUI);
+Vue.use(DatePicker);
+Vue.use(Table);
